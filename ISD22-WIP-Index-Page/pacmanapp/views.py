07ab1e11be150d0895c0aa64aab242264686fe48
SirAdmiral <117938824+SirAdmiral@users.noreply.github.com>
2022-12-19 12:04:16 +0100

    Add files via upload

---
+++ ISD22-WIP-Index-Page/pacmanapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+
+def index(request):
+    """
+    Returns the index.html for testing purposes
+    """
+    return render(request, 'index.html', {})
+
+
+def pacman(request):
+    """
+    Returns the pacman.html
+    """
+    return render(request, 'pacman.html', {})
+
+def profile(requeest):
+    """
+    Returns the profile.html
+    """
+    return render(requeest, 'profile.html', {})
+
+
+def registration(request):
+    return render(request, 'registration.html', {})
