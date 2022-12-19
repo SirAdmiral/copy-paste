12c50403af52b2c0ee4f4ca756657e724275ebd0
SirAdmiral <117938824+SirAdmiral@users.noreply.github.com>
2022-12-19 12:12:18 +0100

    Delete pacmanapp directory

--- pacmanapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('pacman', views.pacman, name='pacman'),
-    path('profile', views.profile, name='profile')
-]
